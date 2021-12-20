4268df00a5997e51850c17f0b4a9a1517906213d
darl4real <darl692000@yahoo.com>
2021-12-20 00:35:36 +0000

    socket.to is added as a function
    function disconnect calls

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ io.on('connection', socket => {
         socket.to(roomId).broadcast.emit('user-connected', userId)
 
         socket.on('disconnect', () => {
-            socket.to(roomId).broadcast.emit('user-disconnected', userId) sess
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
         })
     })
-})
+}) 111
 
 server.listen(3000)
